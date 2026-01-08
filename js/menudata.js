d7c4f554fe265bb63a2071f71f1c2a43972129a2
abhinavtapaskar-afk <abhinavtapaskar@gmail.com>
2026-01-08 16:39:17 +0530

    Update menudata.js

--- js/menudata.js
+++ js/menudata.js
@@ -1,40 +1,63 @@
 const menuData = {
-    "Hot Coffee & Tea": [
+    "Hot Beverages": [
         { id: 1, name: "Special Tea", price: 20 },
         { id: 2, name: "Black Tea", price: 20 },
         { id: 3, name: "Coffee", price: 30 },
         { id: 4, name: "Black Coffee", price: 30 },
-        { id: 5, name: "Bournvita", price: 50 },
-        { id: 6, name: "Hot Chocolate", price: 60 }
+        { id: 5, name: "Hot Chocolate", price: 60 },
+        { id: 6, name: "Bournvita", price: 50 }
     ],
-    "Cold Coffee": [
+    "Cold Coffee & Shakes": [
         { id: 7, name: "Classic Cold Coffee", price: 70 },
         { id: 8, name: "Strong Cold Coffee", price: 80 },
         { id: 9, name: "Thick Cold Coffee", price: 90 },
         { id: 10, name: "Cad-B", price: 100 },
-        { id: 11, name: "Coffee with Icecream", price: 100 }
+        { id: 11, name: "Coffee with Ice Cream", price: 100 },
+        { id: 12, name: "Chocolate Shake", price: 90 },
+        { id: 13, name: "Oreo Shake", price: 110 },
+        { id: 14, name: "Kit-Kat Shake", price: 120 }
     ],
     "Mocktails": [
-        { id: 12, name: "Blue Lagoon", price: 90 },
-        { id: 13, name: "Green Apple", price: 90 },
-        { id: 14, name: "Virgin Mojito", price: 100 },
-        { id: 15, name: "Strawberry Mojito", price: 110 }
+        { id: 15, name: "Blue Lagoon", price: 90 },
+        { id: 16, name: "Green Apple", price: 90 },
+        { id: 17, name: "Virgin Mojito", price: 100 },
+        { id: 18, name: "Strawberry Mojito", price: 110 },
+        { id: 19, name: "Watermelon Mojito", price: 110 },
+        { id: 20, name: "Kiwi Mojito", price: 110 }
     ],
     "Burgers": [
-        { id: 16, name: "Veg Burger", price: 80 },
-        { id: 17, name: "Cheese Burger", price: 100 },
-        { id: 18, name: "Paneer Burger", price: 120 },
-        { id: 19, name: "Maharaja Burger", price: 150 }
-    ],
-    "Pizza & Pasta": [
-        { id: 20, name: "Margherita Pizza", price: 150 },
-        { id: 21, name: "Veggie Feast Pizza", price: 190 },
-        { id: 22, name: "Paneer Tikka Pizza", price: 230 },
-        { id: 23, name: "White Sauce Pasta", price: 140 }
+        { id: 21, name: "Veg Burger", price: 80 },
+        { id: 22, name: "Veg Cheese Burger", price: 100 },
+        { id: 23, name: "Paneer Burger", price: 120 },
+        { id: 24, name: "Maharaja Burger", price: 150 },
+        { id: 25, name: "Chicken Burger", price: 130 },
+        { id: 26, name: "Chicken Cheese Burger", price: 150 }
     ],
     "Sandwiches": [
-        { id: 24, name: "Veg Grilled Sandwich", price: 90 },
-        { id: 25, name: "Cheese Chilli Toast", price: 110 },
-        { id: 26, name: "Paneer Grilled Sandwich", price: 130 }
+        { id: 27, name: "Veg Grilled Sandwich", price: 90 },
+        { id: 28, name: "Veg Cheese Grilled", price: 110 },
+        { id: 29, name: "Cheese Chilli Toast", price: 110 },
+        { id: 30, name: "Paneer Grilled Sandwich", price: 130 },
+        { id: 31, name: "Corn & Cheese Sandwich", price: 120 },
+        { id: 32, name: "Club Sandwich", price: 140 }
+    ],
+    "Pizza": [
+        { id: 33, name: "Margherita Pizza", price: 150 },
+        { id: 34, name: "Veggie Feast Pizza", price: 190 },
+        { id: 35, name: "Paneer Tikka Pizza", price: 230 },
+        { id: 36, name: "Corn & Cheese Pizza", price: 210 },
+        { id: 37, name: "The Bucks Special Pizza", price: 260 }
+    ],
+    "Pasta": [
+        { id: 38, name: "White Sauce Pasta", price: 140 },
+        { id: 39, name: "Red Sauce Pasta", price: 130 },
+        { id: 40, name: "Pink Sauce Pasta", price: 150 }
+    ],
+    "Maggi": [
+        { id: 41, name: "Plain Maggi", price: 50 },
+        { id: 42, name: "Veg Maggi", price: 60 },
+        { id: 43, name: "Cheese Maggi", price: 80 },
+        { id: 44, name: "Paneer Maggi", price: 90 },
+        { id: 45, name: "Schezwan Maggi", price: 70 }
     ]
 };
