bb2b6b68a22326db61a1800ebf920e8c15540b0f
abhinavtapaskar-afk <abhinavtapaskar@gmail.com>
2026-01-08 13:43:07 +0530

    Create menudata.js

---
+++ js/menudata.js
@@ -0,0 +1,40 @@
+const menuData = {
+    "Hot Coffee & Tea": [
+        { id: 1, name: "Special Tea", price: 20 },
+        { id: 2, name: "Black Tea", price: 20 },
+        { id: 3, name: "Coffee", price: 30 },
+        { id: 4, name: "Black Coffee", price: 30 },
+        { id: 5, name: "Bournvita", price: 50 },
+        { id: 6, name: "Hot Chocolate", price: 60 }
+    ],
+    "Cold Coffee": [
+        { id: 7, name: "Classic Cold Coffee", price: 70 },
+        { id: 8, name: "Strong Cold Coffee", price: 80 },
+        { id: 9, name: "Thick Cold Coffee", price: 90 },
+        { id: 10, name: "Cad-B", price: 100 },
+        { id: 11, name: "Coffee with Icecream", price: 100 }
+    ],
+    "Mocktails": [
+        { id: 12, name: "Blue Lagoon", price: 90 },
+        { id: 13, name: "Green Apple", price: 90 },
+        { id: 14, name: "Virgin Mojito", price: 100 },
+        { id: 15, name: "Strawberry Mojito", price: 110 }
+    ],
+    "Burgers": [
+        { id: 16, name: "Veg Burger", price: 80 },
+        { id: 17, name: "Cheese Burger", price: 100 },
+        { id: 18, name: "Paneer Burger", price: 120 },
+        { id: 19, name: "Maharaja Burger", price: 150 }
+    ],
+    "Pizza & Pasta": [
+        { id: 20, name: "Margherita Pizza", price: 150 },
+        { id: 21, name: "Veggie Feast Pizza", price: 190 },
+        { id: 22, name: "Paneer Tikka Pizza", price: 230 },
+        { id: 23, name: "White Sauce Pasta", price: 140 }
+    ],
+    "Sandwiches": [
+        { id: 24, name: "Veg Grilled Sandwich", price: 90 },
+        { id: 25, name: "Cheese Chilli Toast", price: 110 },
+        { id: 26, name: "Paneer Grilled Sandwich", price: 130 }
+    ]
+};
